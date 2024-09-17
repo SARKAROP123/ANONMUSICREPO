070bf01d2af21cf0cc26f11d5bef39b943d091b5
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-09-17 13:07:28 +0530

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -28,7 +28,7 @@ from config import BANNED_USERS, lyrical
 @app.on_message(
     filters.command(
         [
-            "play",
+            "plhhhhhay",
             "vplay",
             "cplay",
             "cvplay",
